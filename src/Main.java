c6783c26b6ea64177d8293e265d41d77b75a1fd6
Relybin <172285502@qq.com>
2017-08-01 03:28:28 +0800

    实现了并归排序
    发现 java好像没法和c++一样引用数组的一部分...

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,19 @@
  */
 public class Main {
 
+    static class Person {
+        int sex = 1;
+    }
+
+    public static void changeSex(Person person) {
+        Person person1 = new Person();
+        person1.sex = 0;
+        person.sex = person1.sex;
+    }
+
     public static void main(String args[]) {
+        Person a = new Person();
+        changeSex(a);
+        System.out.println(a.sex);
     }
 }
--- src/myarithmetic/Sort.java
+++ src/myarithmetic/Sort.java
@@ -8,9 +8,8 @@ public class Sort {
      * 冒泡排序 O(N²)
      *
      * @param list 未排序的数组
-     * @return 排序后的数组
      */
-    public static int[] bubble(int[] list) {
+    public static void bubble(int[] list) {
 
         for (int i = 0; i < list.length; i++) {
             for (int j = 0; j < list.length - 1 - i; j++) {
@@ -22,16 +21,14 @@ public class Sort {
 
             }
         }
-        return list;
     }
 
     /**
      * 插入排序 O(N²)
      *
      * @param list 未排序的数组
-     * @return 排序后的数组
      */
-    public static int[] insertion(int[] list) {
+    public static void insertion(int[] list) {
 
         int j;
         for (int i = 1; i < list.length; i++) {
@@ -48,10 +45,14 @@ public class Sort {
             list[j + 1] = temp1;
 
         }
-        return list;
     }
 
-    public static int[] shell(int[] list) {
+    /**
+     * 希尔排序
+     *
+     * @param list 未排序的数组
+     */
+    public static void shell(int[] list) {
 
         int j;
 
@@ -73,13 +74,67 @@ public class Sort {
             }
 
         }
-        return list;
     }
 
-    public static void main(String[] args) {
-        int[] a = {34, 8, 64, 51, 32, 21};
-        System.out.println(Arrays.toString(shell(a)));
-//        System.out.println(Arrays.toString(bubble(a)));
+    /**
+     * 并归排序递归例程
+     *
+     * @param list 未排序的数组
+     */
 
+    public  static void merge(int[] list){
+        int length = list.length;
+        if (length>2){
+            int[] tempList1 = new int[length/2];
+            System.arraycopy(list, 0, tempList1, 0, tempList1.length);
+            int[] tempList2 = new int[length-length/2];
+            System.arraycopy(list,tempList1.length,tempList2,0,tempList2.length);
+            merge(tempList1);
+            System.arraycopy(tempList1, 0, list, 0, tempList1.length);
+            merge(tempList2);
+            System.arraycopy(tempList2,0,list,tempList1.length,tempList2.length);
+        }
+        mergeA(list);
+    }
+
+    /**
+     * 并归排序具体实现
+     * @param list 数组中分开的两个数组已排序
+     */
+    private static void mergeA(int[] list) {
+        int[] tempList = new int[list.length];
+
+        int left = 0;
+        int center = list.length / 2;
+        int right = center;
+
+        int i = 0;
+        while (left < center && right < list.length) {
+            if (list[left] <= list[right]) {
+                tempList[i++] = list[left++];
+            } else {
+                tempList[i++] = list[right++];
+            }
+        }
+
+        while (left < center) {
+            tempList[i++] = list[left++];
+        }
+
+        while (right < list.length) {
+            tempList[i++] = list[right++];
+        }
+
+        System.arraycopy(tempList, 0, list, 0, list.length);
+
+    }
+
+
+    public static void main(String[] args) {
+//        int[] a = {1,3,2};
+//        int[] a = {1, 3, 5, 7, 9, 2, 4, 6, 8, 10};
+        int[] a = {34,8,64,51,32,21};
+        merge(a);
+        System.out.println(Arrays.toString(a));
     }
 }
